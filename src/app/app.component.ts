58b75af4d388f2de53446d89c96dced2ae1896d0
Shaikh <shaikhdanish3004@gmail.com>
2023-02-03 11:52:22 +0530

    Changes Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -89,49 +89,8 @@ export class AppComponent {
       this.errorMsg=""; 
      this.availData='';
      this.panelList='';
-
     }
   }
-  //getAll.reload();
-
-  // // Data to be Export in Excel 
-
-  // userList = [
-  //   {
-  //     "id": 1,
-  //     "name": "Leanne Graham",
-  //     "username": "Bret",
-  //     "email": "Sincere@april.biz"
-  //   },
-
-  //   {
-  //     "id": 2,
-  //     "name": "Ervin Howell",
-  //     "username": "Antonette",
-  //     "email": "Shanna@melissa.tv"
-  //   },
-
-  //   {
-  //     "id": 3,
-  //     "name": "Clementine Bauch",
-  //     "username": "Samantha",
-  //     "email": "Nathan@yesenia.net"
-  //   },
-
-  //   {
-  //     "id": 4,
-  //     "name": "Patricia Lebsack",
-  //     "username": "Karianne",
-  //     "email": "Julianne.OConner@kory.org"
-  //   },
-
-  //   {
-  //     "id": 5,
-  //     "name": "Chelsey Dietrich",
-  //     "username": "Kamren",
-  //     "email": "Lucio_Hettinger@annie.ca"
-  //   }
-  // ]
 
   exportexcel(): void {
     /* pass here the table id */
